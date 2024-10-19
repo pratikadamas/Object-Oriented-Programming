0d4597ed2822fc46cd9394793b31f38636c60300
pratikadamas <147491382+pratikadamas@users.noreply.github.com>
2024-10-20 03:21:13 +0530

    STL vector

---
+++ vector.cpp
@@ -0,0 +1,17 @@
+//
+// Created by Pratik Giri on 20-10-2024.
+//
+// Online C++ compiler to run C++ program online
+#include <iostream>
+#include<vector>
+using namespace std;
+
+int main() {
+    // Write C++ code here
+    vector<int>v={1,2,3,4,5};
+    cout<<v.size()<<endl;
+    cout<<  v.capacity();
+    std::cout << "Hello, World!";
+
+    return 0;
+}
