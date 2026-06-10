aa59c90cca5dd86253ec5b154f6a0661652297f6
Pratik Giri <pratikgiri585@gmail.com>
2026-06-10 23:36:03 +0530

    Draw floyd triangle

--- floyd .cpp
+++ floyd .cpp
@@ -15,7 +15,6 @@ int main() {
 
     int totalNumbers = (rows * (rows + 1)) / 2;
 
-
     for (int i = rows; i >= 1; i--) {
         for (int j = 1; j <= i; j++) {
             cout << totalNumbers-- << " ";
